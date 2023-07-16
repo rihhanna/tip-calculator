77a03ebce0f264f049c8625d1f0d6c7cf2c80288
rehhana <ashiqtalmustahiil@gmail.com>
2023-07-16 14:16:43 -0700

    answers of tip calculator

--- challenge.js
+++ challenge.js
@@ -1,2 +1,13 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+const bill=100
+const tib =bill*0.15
+const tib2=bill*0.2
+const total=bill+tib
+const total2=bill+tib2
+if(bill>=50&&bill<=300){
+    console.log('the bill was',bill,'the tip was',tib,'the total is',total)
+}else{
+    console.log('the bill was',bill,'the tib was',tib2,'the total is',total2)
+}
+
